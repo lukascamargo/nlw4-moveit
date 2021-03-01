aa2e98a5f35d1700697c541720f6d90ebe02bb2e
Lukas Camargo <lukas.camargo@global.ntt>
2021-02-28 23:58:54 -0300

    level up modal

---
+++ src/components/Profile.tsx
@@ -0,0 +1,19 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
+import styles from '../styles/components/Profile.module.css';
+
+export function Profile() {
+    const { level } = useContext(ChallengesContext);
+    return (
+        <div className={styles.profileContainer}>
+            <img src="https://github.com/lukasfialho.png" alt="Lukas Fialho"/>
+            <div>
+                <strong>Lukas Fialho</strong>
+                <p>
+                    <img src="icons/level.svg" alt="Level"/>
+                    Level {level}
+                </p>
+            </div>
+        </div>
+    );
+}
